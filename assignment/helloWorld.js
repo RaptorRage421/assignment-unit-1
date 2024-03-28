94d474b628e6db5e46d5aa5bdc7e09f36ce673ef
Sam Shacter <sshacter@gmail.com>
2024-03-28 14:04:58 -0500

    made changes to hellowWorld.js and linked to html.

---
+++ assignment/helloWorld.js
@@ -0,0 +1,19 @@
+console.time()
+
+setTimeout(() => {
+  console.timeEnd()
+}, 5000)
+setTimeout(() => {
+    console.timeLog()
+  }, 4000)
+  setTimeout(() => {
+    console.timeLog()
+  }, 3000)
+  setTimeout(() => {
+    console.timeLog()
+  }, 2000)
+  setTimeout(() => {
+    console.timeLog()
+  }, 1000)
+  
+console.log("Hello Instructor! Please enjoy this 5 second timer")
